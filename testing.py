4d3462ab5514ae58a904deac432fa6398a5fef6a
Arielle Isaacs <amisaacs@g.hmc.edu>
2020-04-23 10:05:48 -0700

    final commit

--- testing.py
+++ testing.py
@@ -8,10 +8,6 @@ df = pd.read_excel('DiabetesAtlasCountyData_excel.xlsx', usecols=['Fips', 'Perce
 with open('counties_locations.json') as response:
     counties = json.load(response)
 
-# counties = df['CountyFIPS'].tolist()
-# percents= df['Percentage'].tolist()
-
-
 fig = go.Figure(data=go.Choropleth(
     locations=df['Fips'], # Spatial coordinates
     z = df['Percentage'], # Data to be color-coded
@@ -25,22 +21,4 @@ fig.update_layout(
     geo_scope = 'usa'
 )
 
-# fig = px.choropleth(df, geojson=counties, locations='Fips', color='Percentage',
-#                            color_continuous_scale="Viridis",
-#                            range_color=(0, 80),
-#                            scope="usa", 
-#                            labels={'Percentage':'percent with diabetes'}
-                        #   ) 
-# fig = px.choropleth(df, geojson=counties, locations='Fips', z='Percentage')
-# fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
 fig.show()
-
-# # Metadata
-# fig.update_layout(
-#     title_text = 'Diabetes Frequency by County',
-#     geo_scope='usa', # limite map scope to USA
-# )
-
-# fig.show()
-
-#https://plotly.com/python/choropleth-maps/
